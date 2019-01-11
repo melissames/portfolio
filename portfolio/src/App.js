ef36cb64f334f7588003ec1eb76c879833b6fdfa
github username <github email address>
2019-01-10 21:46:16 -0500

    added react and photo

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import PhotoHeader from './components/PhotoHeader'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PhotoHeader />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ portfolio/src/components/PhotoHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PhotoHeader extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://lh3.googleusercontent.com/WxVv3bPNZ37_Ie38skLMG5H4ZTt5G_Jyr6oQsuhE6Ndo5bBe7kSyat_JwdKqxlh2k-zsWT7iod3bM4NMoh3iu-Z6HQWtneIlpyHlUIKqxuPEwwZnnZwSbxWbtvAiu1tupc6hTmoZKKGGDSAo7xG_aXKLmJSnilq4S1qyRVrYHhoKxn4nkfQr67eQ3vKaQtIJgWQTZ2Ks5fXkQBfEiOkOkYcmYfFFbcR9OgKhd8vi8OqAQL0K3rMqTntMcifBHw8zFHHgArXf0e54zc8j79cFiiUysDtNtrYVXNc40H0-rZGwD5JSM_Xe_05cRNckGl8eYmZeqhtRVi2Hrlb_VRFpQjlzQeUhCGXr3W5RkTMB-rtIArJt0-RVMt7meEzt8dFC_WcHZ50_7ovvsjdIgF9b5FSq9KNaSJ1KDU97tvz-DreZF-JOiVyeXqgELaabmk_ln9GXTmmqJgxgNWFl25OLvZGmF5FYWsb3CbEqePfU_V4VuStq3jcZWE1TwFruCbToQSvty5G3NbF3Woh8ZcJeM0kGhSNoNACUQvJCBsuCX8wVRXDZOTfmmgml18BmjdShGMIqYNEnSMh3DV_a_2gbj9XF7PUEbifXLODUcoKMRx8_r1HUJxxuwtndio8wxPr6QUUbM1S0NeHIpk48tkwuw2tK=w1422-h777-no"
+        alt=""
+        className="photo-header"/>
+      </div>
+    );
+  }
+}
+
+export default PhotoHeader;
