335ebed979936586f5e4b6cc3eb79e0cfccd2176
github username <github email address>
2019-01-14 22:37:30 -0500

    added portfolio images and descriptions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ComingSoon from '../photos/ComingSoon.jpg';
-import PieChart from '../photos/PieChart.png'
+import PXL8 from '../photos/PXL8.png'
 
 class Portfolio extends Component {
   render() {
@@ -8,20 +8,28 @@ class Portfolio extends Component {
       <div className="portfolio-container">
         <h1 className="portfolio-header">Portfolio</h1>
         <h2 className="portfolio-description">I most recently partnered with another Flatiron grad to rewrite bark.co for Bark, creators of Barkbox.  I'm currently freelancing and working on creating my own large scale UI Library using CSS, JS, React, Vue, Angular, and D3.</h2>
+        <br />
         <div className="portfolio-list">
           <div className="portfolio-item">
-            <img src={ComingSoon}></img>
-            <h2>Underground UI</h2>
-            <h3>A vast component Library using CSS, JS, React, Vue, Angular, and D3</h3>
+            <div>
+              <img src={ComingSoon} alt="" className="portfolio-coming-soon"></img>
+              <h2>Underground UI</h2>
+              <h3>A vast component Library using CSS, JS, React, Vue, Angular, and D3</h3>
+            </div>
           </div>
           <div className="portfolio-item">
-            <h2>Bark.co</h2>
-            <h3>A marketing website for users interested in Bark as a company including a home page, careers page, press page, and contact page</h3>
+            <div>
+              <img src="http://g.recordit.co/HqvTY0cAdw.gif" alt="" className="portfolio-bark-co"></img>
+              <h2>Bark.co</h2>
+              <h3>A marketing website for users interested in Bark as a company including a home page, careers page, press page, and contact page</h3>
+            </div>
           </div>
           <div className="portfolio-item">
-            <img src={PieChart}></img>
-            <h2>PXL8</h2>
-            <h3>A React front end and a Rails backend calls on the Artsy API to search artists and returns a Chart.js pie graph of the top 5 colors present in each photo using RMagick</h3>
+            <div>
+              <img src={PXL8} alt="" className="portfolio-pie-chart"></img>
+              <h2>PXL8</h2>
+              <h3>A React front end and a Rails backend calls on the Artsy API to search artists and returns a Chart.js pie graph of the top 5 colors present in each photo using RMagick</h3>
+            </div>
           </div>
         </div>
       </div>
