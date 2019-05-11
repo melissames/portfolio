aca11c1a7481ee4aadc3262c8458dc931af7e507
github username <melissa.sames@gmail.com>
2019-05-11 15:58:21 -0400

    Updates

---
+++ precache-manifest.abc9488e26db61229c28287bbfe17e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea19f0d35309f050760828270ff09abb",
+    "url": "/portfolio-website/static/media/BarkCo.ea19f0d3.gif"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "11519672d44264a1d273",
+    "url": "/portfolio-website/static/js/main.11519672.chunk.js"
+  },
+  {
+    "revision": "1a596ca731a12bb19d37",
+    "url": "/portfolio-website/static/js/1.1a596ca7.chunk.js"
+  },
+  {
+    "revision": "11519672d44264a1d273",
+    "url": "/portfolio-website/static/css/main.42cd9c9b.chunk.css"
+  },
+  {
+    "revision": "b0066837eafede59a1f52f0a32477cdf",
+    "url": "/portfolio-website/index.html"
+  }
+];
