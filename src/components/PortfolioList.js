64b9a5a85c3a150f2ceda0323c1dd21646869174
Melissa Sames <31673951+melissames@users.noreply.github.com>
2019-05-11 14:54:24 -0400

    Portfolio (#5)
    * added portfolio images and descriptions
    
    * added gif locally
    
    * arranging portfolio items
    
    * adding additional changes
    
    * updates ready for launch

---
+++ src/components/PortfolioList.js
@@ -0,0 +1,15 @@
+{/* <div className="portfolio-item-container">
+  <img src={ComingSoon} alt="" className="portfolio-image"></img>
+  <div className="portfolio-description">
+    <h2>Underground UI</h2>
+    <h3>A vast component Library using CSS, JS, React, Vue, Angular, and D3</h3>
+  </div>
+</div>
+
+<div className="portfolio-item-container">
+  <img src={PXL8} alt="" className="portfolio-image"></img>
+  <div className="portfolio-description">
+    <h2>PXL8</h2>
+    <h3>A Rails and React stack calls on the Artsy API to search artists and returns a Chart.js pie graph of the top 5 colors present in each photo using RMagick</h3>
+  </div>
+</div> */}
